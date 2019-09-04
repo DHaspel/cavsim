6d8f03922f7c2674dc323386d135a0d93caa75f8
Dominik Haspel <dominik@dhaspel.de>
2019-09-04 12:37:33 +0200

    implement Pipe class calculation methods

--- cavsim/pipes/pipe.py
+++ cavsim/pipes/pipe.py
@@ -36,7 +36,8 @@ class Pipe(BasePipe):
             length: float,
             wall_thickness: float,
             bulk_modulus: float,
-            roughness: float
+            roughness: float,
+            inner_points: int = None,
     ) -> None:
         """
         Initialization of the class
@@ -46,14 +47,23 @@ class Pipe(BasePipe):
         :param wall_thickness: Thickness of the wall [m]
         :param bulk_modulus: Bulk modulus of wall material [Pa]
         :param roughness: Roughness of the wall [m]
+        :param inner_points: Minimal number of inner points for discretization
         :raises TypeError: Wrong type of at least one parameter
+        :raises ValueError: Value of at least one parameter out of bounds
         """
         super(Pipe, self).__init__(diameter, length, wall_thickness, bulk_modulus, roughness)
+        if not isinstance(inner_points, int):
+            raise TypeError('Wrong type for parameter inner_points ({} != {})'.format(type(inner_points), int))
+        if inner_points is not None and inner_points < 3:
+            raise ValueError('Number of inner points ({}) needs to greater than 2!'.format(inner_points))
         # Register internal fields
+        self._inner_points = inner_points
         self._pressure: np.ndarray = self.field_create('pressure', 3)
         self._velocity: np.ndarray = self.field_create('velocity', 3)
         self.field_create('reynolds', 1)
-        self.field_create('friction_factor', 1)
+        self.field_create('darcy_friction_factor', 1)
+        self.field_create('friction_steady', 1)
+        self.field_create('friction', 1)
         # Create the left connector
         self._left: Connector = Connector(self, [
             ExportChannel(Measure.deltaX, lambda: self._delta_x),
@@ -113,26 +123,35 @@ class Pipe(BasePipe):
 
         :return: Maximum allowed timestep width or None if any is suitable
         """
-        # todo: Calculate max delta t
-        return None
+        n_min = self._inner_points if self._inner_points is not None else 3
+        result = self.length / ((n_min + 1) * self.norm_speed_of_sound)
+        return result
 
     def discretize(self, delta_t: float) -> None:
         """
         Method handling the discretization of the component (for a given timestep width)
 
         :param delta_t: Timestep width to discretize for
+        :raises ValueError: Timestep too large to fit at least 3 inner points
         """
         self._delta_t = delta_t
-        x_count = 5  # todo: discretize the internal states
-        self.fields_resize(x_count)
+        nodes = int(np.ceil(self.length / (self.norm_speed_of_sound * delta_t)) - 1)
+        if nodes < 3:
+            raise ValueError('Timestep to large!')
+        self._delta_x = self.length / float(nodes + 1)
+        self.fields_resize(nodes + 2)
 
     def initialize(self) -> None:
         """
         Initialize the internal state of the component (after discretization was called)
         """
-        self.field('velocity')[:, :] = np.zeros(self._velocity.shape)[:, :]
-        self.field('pressure')[:, :] = self.fluid.norm_pressure * np.ones(self._pressure.shape)[:, :]
-        # todo: Initialize the internal states
+        self.field('velocity')[:, :] = np.zeros(self.field('velocity').shape)[:, :]
+        self.field('pressure')[:, :] = self.fluid.norm_pressure * np.ones(self.field('pressure').shape)[:, :]
+        self.field('reynolds')[:, :] = np.zeros(self.field('reynolds').shape)[:, :]
+        self.field('darcy_friction_factor')[:, :] = np.ones(self.field('darcy_friction_factor').shape)[:, :]
+        self.field('friction_steady')[:, :] = np.zeros(self.field('friction').shape)[:, :]
+        self.field('friction')[:, :] = np.zeros(self.field('friction').shape)[:, :]
+        # todo: User defined initialize of the internal states
 
     def prepare_next_timestep(self, delta_t: float, next_total_time: float) -> None:
         """
@@ -151,7 +170,6 @@ class Pipe(BasePipe):
         self._pressure[1, -1] = self.right.value(Measure.pressureLast)
         self._velocity[1, -1] = -self.right.value(Measure.velocityMinusLast)
         self._velocity[2, -1] = -self.right.value(Measure.velocityMinusLast2)
-        # todo: Other things to prepare each timestep
 
     def prepare_next_inner_iteration(self, iteration: int) -> None:
         """
@@ -159,8 +177,6 @@ class Pipe(BasePipe):
 
         :param iteration: Number of the next inner iteration to prepare for
         """
-        # todo: Exchange current properties with boundaries
-        # todo: Other things to prepare each inner iteration
 
     def calculate_next_inner_iteration(self, iteration: int) -> bool:
         """
@@ -169,5 +185,125 @@ class Pipe(BasePipe):
         :param iteration: Number of the next inner iteration
         :return: Whether this component needs another inner iteration afterwards
         """
-        # todo: Perform the actual calculation
+        self._calculate_reynolds()
+        self._calculate_friction()
+        self._calculate_pressure()
+        self._calculate_velocity()
         return False
+
+    def _calculate_reynolds(self) -> None:
+        """
+        Calculate the Reynolds number based on the values from the previous time step
+        """
+        # Get the input fields
+        pressure = self.field_wide_slice('pressure', 1)
+        velocity = self.field_wide_slice('velocity', 1)
+        # Calculate fluid properties
+        viscosity = self.fluid.viscosity(temperature=None, shear_rate=None)
+        density = self.fluid.density(pressure=pressure, temperature=None)
+        # Calculate the reynolds number
+        result = (density * velocity * self.diameter) / viscosity
+        # Store/return the calculated result
+        self.field_wide_slice('reynolds')[:] = result[:]
+
+    def _calculate_darcy_friction_factor(self) -> None:
+        """
+        Calculates darcy's friction coefficient within the pipe
+        """
+        # Get the input fields
+        reynolds = self.field_wide_slice('reynolds')
+        result = np.ones(reynolds.shape)
+        # Calculate the friction factor (low Re)
+        selector = np.logical_and(reynolds > 0.0, reynolds < 2100.0)
+        if np.sum(selector) > 0:
+            local_reynolds = reynolds[selector]
+            factor = 64.0 / local_reynolds
+            result[selector] = factor
+        # Calculate the friction factor (high Re)
+        selector = (reynolds >= 2100.0)
+        if np.sum(selector) > 0:
+            local_reynolds = reynolds[selector]
+            factor = 10.0 * np.ones(local_reynolds.shape)
+            error = np.ones(local_reynolds.shape)
+            while np.any(error > 1e-12):
+                term1 = self.roughness / (3.7 * self.diameter)
+                term2 = 2.51 / (local_reynolds * np.sqrt(factor))
+                temp = -2.0 * np.log10(term1 + term2)
+                old_factor, factor = factor, np.square(1.0 / temp)
+                error = np.abs(factor - old_factor)
+            result[selector] = factor
+        # Store/return the calculated result
+        self.field_wide_slice('darcy_friction_factor')[:] = result[:]
+
+    def _calculate_friction_steady(self) -> None:
+        """
+        Calculate the steady friction using darcy's factor
+        """
+        # Get the input fields
+        velocity = self.field_wide_slice('velocity', 1)
+        friction_factor = self.field_wide_slice('darcy_friction_factor', 0)
+        # Calculate the friction
+        result = (friction_factor / (2.0 * self.diameter)) * np.abs(velocity)
+        # Store/return the calculated result
+        self.field_wide_slice('friction_steady')[:] = result[:]
+
+    def _calculate_friction(self) -> None:
+        """
+        Calculate the total friction (steady + unsteady)
+        """
+        self._calculate_darcy_friction_factor()
+        self._calculate_friction_steady()
+        # todo: calculate unsteady friction
+        friction_steady = self.field_wide_slice('friction_steady')
+        result = friction_steady
+        self.field_wide_slice('friction')[:] = result[:]
+
+    def _calculate_pressure(self) -> None:
+        """
+        Calculate the pressure of the current time step
+        """
+        # Get the input fields
+        pressure_center = self.field_slice('pressure', 1, 0)
+        pressure_a = self.field_slice('pressure', 1, -1)
+        pressure_b = self.field_slice('pressure', 1, +1)
+        velocity_a = self.field_slice('velocity', 1, -1)
+        velocity_b = self.field_slice('velocity', 1, +1)
+        friction_a = self.field_slice('friction', 0, -1)
+        friction_b = self.field_slice('friction', 0, +1)
+        # Calculate fluid properties
+        speed_of_sound = self.speed_of_sound(pressure=pressure_center, temperature=None)
+        density = self.fluid.density(pressure=pressure_center, temperature=None)
+        # Calculate the reynolds number
+        result = 0.5 * (
+            (speed_of_sound * density * (velocity_a - velocity_b))
+            + (pressure_a + pressure_b)
+            + (self._delta_t * speed_of_sound * density * (friction_b - friction_a))
+            # todo: height terms
+        )
+        # Store/return the calculated result
+        self.field_slice('pressure', 0, 0)[:] = result[:]
+
+    def _calculate_velocity(self) -> None:
+        """
+        Calculate the velocity of the current time step
+        """
+        # Get the input fields
+        pressure_center = self.field_slice('pressure', 1, 0)
+        pressure_a = self.field_slice('pressure', 1, -1)
+        pressure_b = self.field_slice('pressure', 1, +1)
+        velocity_a = self.field_slice('velocity', 1, -1)
+        velocity_b = self.field_slice('velocity', 1, +1)
+        friction_a = self.field_slice('friction', 0, -1)
+        friction_b = self.field_slice('friction', 0, +1)
+        # Calculate fluid properties
+        speed_of_sound = self.speed_of_sound(pressure=pressure_center, temperature=None)
+        density = self.fluid.density(pressure=pressure_center, temperature=None)
+        # Calculate the reynolds number
+        result = 0.5 * (
+            (velocity_a + velocity_b)
+            + ((1.0 / (speed_of_sound * density)) * (pressure_a - pressure_b))
+            - (self._delta_t * (friction_a + friction_b))
+            # todo: height terms
+        )
+        # Store/return the calculated result
+        self.field_slice('velocity', 0, 0)[:] = result[:]
